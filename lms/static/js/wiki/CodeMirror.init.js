dc833df9a5a96723d357dc180f59871f4d19c741
cahrens <christina@edx.org>
2017-01-30 11:08:19 -0500

    Accessibility fixes and tests.
    TNL-6369, TNL-6368

--- lms/static/js/wiki/CodeMirror.init.js
+++ lms/static/js/wiki/CodeMirror.init.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
   // Store the inital contents so we can compare for unsaved changes
     var initial_contents = editor.getValue();
 
+    // The Wiki associates a label with the text area that has ID id_content. However, when we swap in
+    // CodeMirror, that text area is hidden. We need to associate the label with CodeMirror (and there
+    // is JS code in the wiki that depends on "id_content" to get the content).
+    editor.getInputField().setAttribute('id', 'id_codemirror_content');
+    $(".control-label[for='id_content']")[0].setAttribute('for', 'id_codemirror_content');
     window.onbeforeunload = function askConfirm() { // Warn the user before they navigate away
         if (editor.getValue() != initial_contents) {
             return 'You have made changes to the article that have not been saved yet.';
